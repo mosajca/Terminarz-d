13f4ddec198e2cc50a6b82617de3315f5c145883
Marcin Osajca <marcin9608@gmail.com>
2019-01-12 19:59:49 +0100

    add update and delete events

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from main.views import EventListView, EventCreateView
+from main.views import EventListView, EventCreateView, EventUpdateView, EventDeleteView
 
 urlpatterns = [
     path('events/', EventListView.as_view()),
     path('add/', EventCreateView.as_view()),
+    path('update/<int:pk>', EventUpdateView.as_view()),
+    path('delete/<int:pk>', EventDeleteView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from main.models import Event
 
 
-class EventListView(ListView):
+class EventListView(LoginRequiredMixin, ListView):
     model = Event
+    login_url = '/login'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
 
 
 class EventCreateView(LoginRequiredMixin, CreateView):
@@ -16,3 +20,16 @@ class EventCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    fields = ['name', 'description', 'start_date_time', 'end_date_time']
+    login_url = '/login'
+    success_url = '/events'
+    template_name_suffix = '_update_form'
+
+
+class EventDeleteView(LoginRequiredMixin, DeleteView):
+    model = Event
+    success_url = '/events'
