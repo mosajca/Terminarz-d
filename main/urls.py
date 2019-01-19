7dae14b9b9994e7b9130d4e72989dcb5ddbc5ae9
Marcin Osajca <marcin9608@gmail.com>
2019-01-19 21:04:01 +0100

    add showing events by day and week

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from main.views import EventListView, EventCreateView, EventUpdateView, EventDeleteView, EventTemplateView
+from main.views import *
 
 urlpatterns = [
     path('', EventTemplateView.as_view()),
     path('events/', EventListView.as_view()),
+    path('events/week/', EventListViewWeek.as_view()),
     path('add/', EventCreateView.as_view()),
     path('update/<int:pk>', EventUpdateView.as_view()),
     path('delete/<int:pk>', EventDeleteView.as_view()),
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
 from main.models import Event
+import datetime
 
 
 class EventTemplateView(TemplateView):
@@ -10,9 +12,52 @@ class EventTemplateView(TemplateView):
 class EventListView(LoginRequiredMixin, ListView):
     model = Event
     login_url = '/login'
+    current = 0
+    day = None
 
     def get_queryset(self):
-        return super().get_queryset().filter(user=self.request.user)
+        try:
+            self.current = int(self.request.GET.get('n', 0))
+        except:
+            self.current = 0
+        self.day = timezone.make_aware(datetime.datetime.today() + datetime.timedelta(days=self.current))
+        return super().get_queryset().filter(user=self.request.user).filter(start_date_time__date=self.day)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['day'] = self.day.date()
+        context['prev'] = self.current - 1
+        context['next'] = self.current + 1
+        return context
+
+
+class EventListViewWeek(LoginRequiredMixin, ListView):
+    model = Event
+    login_url = '/login'
+    current = 0
+    start_week = None
+    end_week = None
+
+    def get_queryset(self):
+        try:
+            self.current = int(self.request.GET.get('n', 0))
+        except:
+            self.current = 0
+        today = datetime.date.today()
+        monday = today - datetime.timedelta(days=today.weekday() - self.current * 7)
+        sunday = monday + datetime.timedelta(days=6)
+        self.start_week = timezone.make_aware(datetime.datetime.combine(monday, datetime.time.min))
+        self.end_week = timezone.make_aware(datetime.datetime.combine(sunday, datetime.time.max))
+        return super().get_queryset().filter(user=self.request.user).filter(
+            start_date_time__range=(self.start_week, self.end_week))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['start_week'] = self.start_week.date()
+        context['end_week'] = self.end_week.date()
+        context['prev'] = self.current - 1
+        context['next'] = self.current + 1
+        return context
 
 
 class EventCreateView(LoginRequiredMixin, CreateView):
